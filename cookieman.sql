cf40a6b33ae6f1f8cd275ff17a173ecffdfc12cd
abxian <111940309+abxian@users.noreply.github.com>
2024-04-29 01:07:02 +0800

    Add files via upload

---
+++ cookieman.sql
@@ -0,0 +1,45 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 1
+ Source Server Type    : MySQL
+ Source Server Version : 101103
+ Source Host           : bt.010199.xyz:3306
+ Source Schema         : cookieman
+
+ Target Server Type    : MySQL
+ Target Server Version : 101103
+ File Encoding         : 65001
+
+ Date: 28/04/2024 17:04:38
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user_cookies
+-- ----------------------------
+DROP TABLE IF EXISTS `user_cookies`;
+CREATE TABLE `user_cookies`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NULL DEFAULT NULL,
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `cookies` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `user_id`(`user_id` ASC) USING BTREE,
+  CONSTRAINT `user_cookies_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
